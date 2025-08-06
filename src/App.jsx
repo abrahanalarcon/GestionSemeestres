ac09429c4bcc783ea47dc6ebaf1ff2824a118176
abrahanalarcon <moiseshazim@gmail.com>
2025-08-05 21:42:56 -0400

    primer comit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-center p-6">
+    <h1 className="text-4xl font-bold text-blue-600 mb-4">
+      Gestión de Semestres
+    </h1>
+    <p className="text-lg text-gray-700 max-w-md">
+      Bienvenido a la aplicación de gestión de semestres.
+    </p>
+  </div>
+  )
+}
+
+export default App
