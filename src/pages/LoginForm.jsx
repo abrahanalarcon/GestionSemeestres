9938b3b0a8c9a71a9ba326eced43f832d54b82dd
abrahanalarcon <moiseshazim@gmail.com>
2025-08-06 15:38:09 -0400

    arraglando sensibilid case

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../context/useAuth";
 
-export default function LoginForm() {
+export default function loginform() {
   const { login } = useAuth();
   const [form, setForm] = useState({ username: "", password: "" });
 
